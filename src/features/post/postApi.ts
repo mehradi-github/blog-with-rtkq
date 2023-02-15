5a97c62993601efcc8e5f58885c9588f6d09c637
Mehradi <mehradi.info@gmail.com>
2023-02-15 10:14:17 +0200

    Adding useGetPostsQuery

--- src/features/post/postApi.ts
+++ src/features/post/postApi.ts
@@ -1,6 +1,27 @@
+import { api } from '../../app/services/api';
+
 export interface Post {
   id: number;
   title: string;
   body: string;
   fetched_at: string;
 }
+
+type PostResponse = Post[];
+
+export const postApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getPosts: build.query<PostResponse, void>({
+      query: () => ({ url: 'posts' }),
+      providesTags: (result = []) => [
+        ...result.map(({ id }) => ({ type: 'Posts', id } as const)),
+        { type: 'Posts' as const, id: 'LIST' },
+      ],
+    }),
+  }),
+});
+
+export const {
+  useGetPostsQuery,
+  endpoints: { getPosts },
+} = postApi;
